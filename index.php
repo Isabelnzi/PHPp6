4e70bc514556d577eff8d164b32e71754b824fa2
Nzingani <isabelnzingani@outlook.com>
2018-07-25 21:40:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exo6</title>
+  </head>
+  <body>
+  <p>
+    <?php
+    $name = 'Ilario';
+    echo 'Bonjour '.$name.', comment vas tu ?';
+    ?>
+  </p>
+  </body>
